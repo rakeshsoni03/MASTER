95a1d2a510af0183101c98cf537ecd1ebe322dd6
RAKESH <RAKESH@DESKTOP-4CIT6DT>
2018-02-09 01:09:48 +0530

    test

---
+++ src/test/java/com/org/learnMaven/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.org.learnMaven;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest {
+	@Test
+	public void Login() 
+	{
+		System.out.println("this is login method");
+	}
+
+}
