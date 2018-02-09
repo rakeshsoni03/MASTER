3d85741fa86552e32308cf00a50de54816c4d2df
RAKESH <RAKESH@DESKTOP-4CIT6DT>
2018-02-09 11:34:34 +0530

    testng.xml push
    Change-Id: Iecfb0d5847ce3a40f80074eead45282676875486

--- src/test/java/com/org/learnMaven/HelloWorldTest.java
+++ src/test/java/com/org/learnMaven/HelloWorldTest.java
@@ -2,6 +2,7 @@ package com.org.learnMaven;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
 public class HelloWorldTest {
@@ -9,7 +10,7 @@ public class HelloWorldTest {
 	public void Login() 
 	{
 		System.out.println("this is login method");
-		WebDriver driver=new FirefoxDriver();
+		WebDriver driver=new InternetExplorerDriver();
 	}
 
 }
