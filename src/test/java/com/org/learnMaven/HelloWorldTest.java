dff7e3fe10ddfd593d1a485141baeb6b43e8b6c5
RAKESH <RAKESH@DESKTOP-4CIT6DT>
2018-02-13 21:17:22 +0530

    webdriver system property set
    Change-Id: I18f0463877bde28fe2960c2795ac36758553b96f

--- src/test/java/com/org/learnMaven/HelloWorldTest.java
+++ src/test/java/com/org/learnMaven/HelloWorldTest.java
@@ -1,6 +1,8 @@
 package com.org.learnMaven;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
@@ -10,7 +12,14 @@ public class HelloWorldTest {
 	public void Login() 
 	{
 		System.out.println("this is login method");
-		WebDriver driver=new InternetExplorerDriver();
+		String executionPath = System.getProperty("user.dir");
+		System.out.println("path is "+executionPath);
+		System.setProperty("webdriver.ie.driver", executionPath+"\\"+"chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://coindelta.com/market/?active=XRP-INR");
+		driver.findElement(By.xpath("//*[@id='market']/section/div[2]/div[2]/div[1]/div/div/div[2]/table/tbody/tr[6]/td[1]")).click();
+		
 	}
 
 }
